858759af61b589533263afe4c56e06383553a7dc
Jaypee <jaypee.d@triuneelectronics.com>
2018-04-19 14:12:24 +0800

    Fixed bug when a user left the room!

--- chatime-node/Chatnode.js
+++ chatime-node/Chatnode.js
@@ -16,104 +16,106 @@ console.log('Chat server is running ...');
 
 io.on('connection', (socket) => {
     connections.push(socket);
-    console.log('Connected: %s sockets connected', connections.length);
-    io.sockets.connected[socket.conn.id].emit('userUniqueId', {
+console.log('Connected: %s sockets connected', connections.length);
+io.sockets.connected[socket.conn.id].emit('userUniqueId', {
+    id: socket.conn.id,
+});
+
+// Disconnect
+socket.on('disconnect', (data) => {
+    if(socket.username) {
+    let user_index = users.findIndex(user => user.name === socket.username);
+    users.splice(user_index, 1);
+    updateUsernames();
+    userExitChat();
+}
+connections.splice(connections.indexOf(socket), 1);
+console.log('Connected: %s sockets connected', connections.length);
+});
+
+// Send Message
+socket.on('send_message', (data) => {
+    let every_mess = {
         id: socket.conn.id,
-    });
+        name: socket.username,
+        message: data.message,
+        user_join: false,
+        user_out: false
+    };
 
-    // Disconnect
-    socket.on('disconnect', (data) => {
-        let user_index = users.findIndex(user => user.name === socket.username);
-        users.splice(user_index, 1);
-        updataUsernames();
-        userExitChat();
-        connections.splice(connections.indexOf(socket), 1);
-        console.log('Connected: %s sockets connected', connections.length);
-    });
+messages.push(every_mess);
 
-    // Send Message
-    socket.on('send_message', (data) => {
-        let every_mess = {
-            id: socket.conn.id,
-            name: socket.username,
-            message: data.message,
-            user_join: false,
-            user_out: false
-        };
+io.sockets.emit('newMessage', {
+    id: socket.conn.id,
+    name: socket.username,
+    message: data.message,
+    user_join: false,
+    user_out: false
+})
+});
 
-        messages.push(every_mess);
+//New User
+socket.on('new_user', (data, callback) => {
+    callback(true);
+socket.username = data.nickname;
+let user_arr = {
+    name: socket.username,
+    id: socket.conn.id,
+    connect: true
+}
+users.push(user_arr);
+updateUsernames();
+userJoinChat();
+getMessages();
+})
 
-        io.sockets.emit('newMessage', {
-            id: socket.conn.id,
-            name: socket.username,
-            message: data.message,
-            user_join: false,
-            user_out: false
-        })
-    });
+let updateUsernames = () => {
+    io.sockets.emit('getUsers', users);
+}
 
-    //New User
-    socket.on('new_user', (data, callback) => {
-        callback(true);
-        socket.username = data.nickname;
-        let user_arr = {
-            name: socket.username,
-            id: socket.conn.id,
-            connect: true
-        }
-        users.push(user_arr);
-        updataUsernames();
-        userJoinChat();
-        getMessages();
+let getMessages = () => {
+    io.sockets.connected[socket.conn.id].emit('retrieveMessages', messages);
+}
+
+let userJoinChat = () => {
+    let user_join_arr = {
+        id: socket.conn.id,
+        name: socket.username,
+        message: `${socket.username} joined the room`,
+        user_join: true,
+        user_out: false
+    };
+
+    messages.push(user_join_arr);
+
+    io.sockets.emit('newMessage', {
+        id: socket.conn.id,
+        name: socket.username,
+        message: `${socket.username} joined the room`,
+        user_join: true,
+        user_out: false
     })
+}
 
-    let updataUsernames = () => {
-        io.sockets.emit('getUsers', users);
-    }
+let userExitChat = () => {
+    let user_exit_arr = {
+        id: socket.conn.id,
+        name: socket.username,
+        message: `${socket.username} left the room`,
+        user_out: true,
+        user_join: false
+    };
 
-    let getMessages = () => {
-        io.sockets.connected[socket.conn.id].emit('retrieveMessages', messages);
-    }
+    messages.push(user_exit_arr);
 
-    let userJoinChat = () => {
-        let user_join_arr = {
-            id: socket.conn.id,
-            name: socket.username,
-            message: `${socket.username} joined the room`,
-            user_join: true,
-            user_out: false
-        };
-
-        messages.push(user_join_arr);
-
-        io.sockets.emit('newMessage', {
-            id: socket.conn.id,
-            name: socket.username,
-            message: `${socket.username} joined the room`,
-            user_join: true,
-            user_out: false
-        })
-    }
-
-    let userExitChat = () => {
-        let user_exit_arr = {
-            id: socket.conn.id,
-            name: socket.username,
-            message: `${socket.username} leaved the room`,
-            user_out: true,
-            user_join: false
-        };
-
-        messages.push(user_exit_arr);
-
-        io.sockets.emit('newMessage', {
-            id: socket.conn.id,
-            name: socket.username,
-            message: `${socket.username} leaved the room`,
-            user_out: true,
-            user_join: false
-        })
-    }
+    io.sockets.emit('newMessage', {
+        id: socket.conn.id,
+        name: socket.username,
+        message: `${socket.username} left the room`,
+        user_out: true,
+        user_join: false
+    })
+}
 });
 
 
